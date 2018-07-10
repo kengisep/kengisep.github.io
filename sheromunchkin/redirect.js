cd4818fc10c20dbb617d39dced760dcb121e1d41
kengisep <32988999+kengisep@users.noreply.github.com>
2018-07-10 03:23:05 +0300

    Add files via upload

---
+++ sheromunchkin/redirect.js
@@ -0,0 +1,19 @@
+<script type="text/javascript">
+var userPlatform = navigator.platform;
+switch(userPlatform) {
+  case 'Android':
+    //location="http://my-website.com/android.apk";
+	alert(andr);
+    [break]
+
+  case 'iPhone':
+    //location="http://my-website.com/iphone.dmg";
+	alert(iph)
+    [break]
+
+  default:
+    //location="http://my-website.com/download.php";
+	alert(userPlatform);
+    [break]
+}
+</script>
